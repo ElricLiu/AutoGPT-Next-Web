cafad1f453a51ea78cbc649e94ab96230c677097
sindlinger <candeia.goncalves@gmail.com>
2024-09-21 03:10:13 -0300

    chore: revert GPTS-Copilot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/env sh
+
+#!/bin/sh
 
 # change schema.prisma
 sed -ie 's/mysql/sqlite/g' prisma/schema.prisma
@@ -6,8 +7,10 @@ sed -ie 's/@db.Text//' prisma/schema.prisma
 
 # Add Prisma and generate Prisma client
 npx prisma generate
+
 # Generate db when not exists
-if [[ ! -f "/app/prisma/${DATABASE_URL:5}" ]]; then
+db_file="/app/prisma/${DATABASE_URL:5}"
+if [ ! -f "$db_file" ]; then
   npx prisma migrate dev --name init
   npx prisma db push
 fi
